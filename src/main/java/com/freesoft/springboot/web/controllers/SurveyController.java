92a7bfc1ac1dbccab2c0cf0eecd10cedb781b6dc
dinabogdan <bogdan.dina03@gmail.com>
2018-02-04 21:12:54 +0200

    retrieve questions REST method

---
+++ src/main/java/com/freesoft/springboot/web/controllers/SurveyController.java
@@ -0,0 +1,24 @@
+package com.freesoft.springboot.web.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.freesoft.springboot.web.beans.Question;
+import com.freesoft.springboot.web.services.SurveyService;
+
+@RestController
+public class SurveyController {
+
+	@Autowired
+	private SurveyService surveyService;
+
+	@GetMapping("/surveys/{surveyId}/questions")
+	public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
+		return surveyService.retrieveQuestions(surveyId);
+	}
+
+}
