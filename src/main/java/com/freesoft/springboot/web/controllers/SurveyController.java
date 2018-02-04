a5270dda8d7bbb367d035a605a77ecc3a6055b25
dinabogdan <bogdan.dina03@gmail.com>
2018-02-04 21:34:41 +0200

    add new REST service & spring-boot-developer-tools dependency

--- src/main/java/com/freesoft/springboot/web/controllers/SurveyController.java
+++ src/main/java/com/freesoft/springboot/web/controllers/SurveyController.java
@@ -21,4 +21,9 @@ public class SurveyController {
 		return surveyService.retrieveQuestions(surveyId);
 	}
 
+	@GetMapping("/surveys/{surveyId}/questions/{questionId}")
+	public Question retrieveQuestionById(@PathVariable String surveyId, @PathVariable String questionId) {
+		return surveyService.retrieveQuestion(surveyId, questionId);
+	}
+
 }
