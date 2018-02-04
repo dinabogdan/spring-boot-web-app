914eaa7201b60c16cbe74578d4539ff2e392f673
dinabogdan <bogdan.dina03@gmail.com>
2018-02-04 22:06:35 +0200

    post REST service for add new question to survey

--- src/main/java/com/freesoft/springboot/web/controllers/SurveyController.java
+++ src/main/java/com/freesoft/springboot/web/controllers/SurveyController.java
@@ -1,11 +1,16 @@
 package com.freesoft.springboot.web.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.freesoft.springboot.web.beans.Question;
 import com.freesoft.springboot.web.services.SurveyService;
@@ -24,6 +29,14 @@ public class SurveyController {
 	@GetMapping("/surveys/{surveyId}/questions/{questionId}")
 	public Question retrieveQuestionById(@PathVariable String surveyId, @PathVariable String questionId) {
 		return surveyService.retrieveQuestion(surveyId, questionId);
+	}
+
+	@PostMapping("/surveys/{surveyId}/questions")
+	public ResponseEntity<Void> addQuestionToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
+		Question question = surveyService.addQuestion(surveyId, newQuestion);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(question.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
