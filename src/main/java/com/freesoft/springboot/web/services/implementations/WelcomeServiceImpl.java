f8b8f5dc0c0ff1f62652aa9a320494284ec51ca0
dinabogdan <bogdan.dina03@gmail.com>
2018-02-10 16:42:33 +0200

    add application.yaml

--- src/main/java/com/freesoft/springboot/web/services/implementations/WelcomeServiceImpl.java
+++ src/main/java/com/freesoft/springboot/web/services/implementations/WelcomeServiceImpl.java
@@ -1,15 +1,19 @@
 package com.freesoft.springboot.web.services.implementations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.freesoft.springboot.web.services.WelcomeService;
 
 @Service
 public class WelcomeServiceImpl implements WelcomeService{
+	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
 
 	@Override
 	public String getWelcomeMessage() {
-		return "Welcome Spring!";
+		return welcomeMessage;
 	}
 
 }
