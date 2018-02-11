20eedfe97a6c806728bb2f7d805ae53c3bec02ba
dinabogdan <bogdan.dina03@gmail.com>
2018-02-11 10:56:01 +0200

    Integration Tests

--- src/test/java/com/freesoft/springboot/web/controllers/SuveyControllerIntegrationTest.java
+++ src/test/java/com/freesoft/springboot/web/controllers/SuveyControllerIntegrationTest.java
@@ -1,11 +1,20 @@
 package com.freesoft.springboot.web.controllers;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.freesoft.springboot.web.Application;
@@ -16,10 +25,30 @@ public class SuveyControllerIntegrationTest {
 
 	@LocalServerPort
 	private int port;
-	
+
+	@Test
+	public void testJsonAssert() {
+		final String url = "http://localhost:" + port + "/surveys/Survey1/questions/Question1";
+		TestRestTemplate restTemplate = new TestRestTemplate();
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(null, httpHeaders);
+		ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+		JSONAssert.assertEquals(
+				"{\"id\":\"Question1\",\"description\":\"Largest Country in the World\",\"correctAnswer\":\"Russia\",\"options\":[\"India\",\"Russia\",\"United States\",\"China\"]}",
+				response.getBody(), false);
+	}
+
 	@Test
 	public void test() {
-		System.out.println("### PORT: " + port);
+		final String url = "http://localhost:" + port + "/surveys/Survey1/questions/Question1";
+		TestRestTemplate restTemplate = new TestRestTemplate();
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(null, httpHeaders);
+		ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+		System.out.println("### RESPONSE: " + response.getBody());
+		assertTrue(response.getBody().contains("\"id\":\"Question1\""));
 	}
 
 }
